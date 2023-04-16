170d398d05da7de843e089412801a9a511f07df1
MPlexVA <mplexva@gmail.com>
2023-04-16 19:27:50 -0400

    Initalize Firebase project from CLI

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -13,6 +13,8 @@ export class AuthService {
     private router: Router
   ) {}
 
+  // TODO: Subscribe to Auth events, redirect to /login upon logout
+
   public sign_in_with_email_pass(email: string, password: string): Promise<UserCredential>  {
     return signInWithEmailAndPassword(this.auth, email, password)
   }
