499969b07c03b4b63879c5a6a4b54cf0b997f6bc
MPlexVA <mplexva@gmail.com>
2023-04-16 20:47:08 -0400

    Firestore Rules. Firestore delete, update arrays

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -2,16 +2,18 @@ import { Component, inject, OnDestroy } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { CommonModule } from '@angular/common';
 import { AuthService } from '../services/auth.service';
-import { Firestore, addDoc, CollectionReference, collection, collectionData, DocumentReference, Timestamp, query, orderBy, Query } from '@angular/fire/firestore';
+import { Firestore, addDoc, CollectionReference, collection, collectionData, DocumentReference, Timestamp, query, orderBy, Query, doc, deleteDoc, updateDoc, arrayUnion, arrayRemove } from '@angular/fire/firestore';
 import { Observable, Subscription } from 'rxjs';
 import { Auth, user, User } from '@angular/fire/auth';
 import { limit } from '@firebase/firestore';
 import { FormControl, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 interface Message {
+  id?: string
   user_uid: string
   display_name: string
   text: string
+  likes?: [string]
   created_at: Timestamp
 }
 
@@ -40,7 +42,7 @@ export class Tab1Page implements OnDestroy {
 
   constructor() {
     this.messagesCollection = collection(this.firestore, 'messages');
-    this.messages$ = collectionData(query(this.messagesCollection, orderBy('created_at', 'desc'), limit(30))) as Observable<Message[]>;
+    this.messages$ = collectionData(query(this.messagesCollection, orderBy('created_at', 'desc'), limit(30)), { idField: 'id'}) as Observable<Message[]>;
 
     this.userSubscription = this.user$.subscribe((user: User | null) => {
       this.current_user = user
@@ -61,8 +63,37 @@ export class Tab1Page implements OnDestroy {
       }
       addDoc(this.messagesCollection, message).then((documentReference: DocumentReference) => {
           // the documentReference provides access to the newly created document
+          // Clear input after success
+          this.message_text.reset()
       });
     }
+  }
+
+  async deleteMessage(id: string | undefined) {
+    if (id) await deleteDoc(doc(this.firestore, 'messages', id))
+  }
+
+  async likeMessage(id: string | undefined) {
+    if (id && this.current_user?.uid) {
+      await updateDoc(doc(this.firestore, 'messages', id), {
+        likes: arrayUnion(this.current_user?.uid)
+      })
+    }
+  }
+
+  async unlikeMessage(id: string | undefined) {
+    if (id && this.current_user?.uid) {
+      await updateDoc(doc(this.firestore, 'messages', id), {
+        likes: arrayRemove(this.current_user?.uid)
+      })
+    }
+  }
+
+  public hasLiked(message: Message) {
+    if (this.current_user?.uid && message.likes?.length) {
+      return message.likes.includes(this.current_user.uid)
+    }
+    return false
   }
 
   public logout() {
